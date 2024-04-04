11aa0034c803366f2ef95fa91fa344abb93d47df
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-04-04 00:11:49 -0700

    Step 120 of the HTML course

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,1 +1,9 @@
+// Step 108
 alert("Hello, World! This is a popup box!");
+
+// Step 120
+function My_Function() {
+    var String = "☘️ Happy Saint Patty's Day! ☘️";
+    var result = String.fontcolor("green");
+    document.getElementById("Irish").innerHTML = result;
+}
