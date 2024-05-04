1f263e86635e002ae13588bba6a7e56837589751
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-05-03 23:54:26 -0700

    Fulfills adding comments assignment

--- JavaScript_Course/Project1_expressions_alert/Functions.js
+++ JavaScript_Course/Project1_expressions_alert/Functions.js
@@ -1,4 +1,4 @@
-function My_First_Function() {
-    let string = 'This is the button text!';
-    document. getElementById("Button_Text").innerHTML = string;
+function My_First_Function() {                                  // Defines a funtion and names it
+    let string = 'This is the button text!';                    // Defines a variable and gives it a string value
+    document. getElementById("Button_Text").innerHTML = string; // Puts the value of the variable into the HTML elementFromPoint with the "Button_Text" id
 }
