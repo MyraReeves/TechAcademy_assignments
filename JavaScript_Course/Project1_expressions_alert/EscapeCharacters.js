98432e85dcbdbf4aa9d31e7ab766fb7242488fdb
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-05-07 20:24:41 -0700

    Adds assignment instructions

--- JavaScript_Course/Project1_expressions_alert/EscapeCharacters.js
+++ JavaScript_Course/Project1_expressions_alert/EscapeCharacters.js
@@ -1,1 +1,6 @@
+// ESCAPE CHARACTER ASSIGNMENT
+
+// Write a string that contains an apostrophe and/or quotation marks. Ensure to escape the applicable character(s).
+
+
 document.write('Lisa told Bart, "Knock it off, Bart! Or I\'ll tell dad!" <br> "Eat my shorts" Bard replied!');
