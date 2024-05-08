3e8c0a1771c014e4d31c856bf05c63cdbeb4f479
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-05-07 21:03:37 -0700

    Adds comments

--- JavaScript_Course/Project1_expressions_alert/Expressions.js
+++ JavaScript_Course/Project1_expressions_alert/Expressions.js
@@ -1,1 +1,23 @@
-Document.write(3+3);
+// Expressions are numbers, symbols, and operators grouped together that show the amount of something. An expression is a written math problem.
+
+// In coding, an expression is a combination of values that are computed by the computer. There are different ways to write out expressions, depending on which language you are programming in.
+
+// For example: Name = “What the person types in the name box” could be an expression. Also, x + 5 is an expression.
+
+// Statements are computer instructions. These are the instructions that are used by people as they create computer programs. The simplest of these might be things like “print,” “delete,” “add,” “multiply,” etc.
+
+// In JavaScript, expressions and statements are different things. An expression results in a value, while a statement performs a task – a program is basically a series of statements. There are basically two types of expressions: ones that have a value (or result in a value) and ones that assign a value to a variable.
+
+// The following would be an expression:
+// 3 + 3
+
+
+
+// Consider the following:
+// Document.write(3 + 3);
+
+// That is a statement containing an expression (3 + 3). As a note: semicolons separate statements.
+
+
+
+Document.write(20+7);
