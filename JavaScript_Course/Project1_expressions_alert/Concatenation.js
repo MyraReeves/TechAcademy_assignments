04271d083385cb0023318463f18d5874bce05955
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-05-07 20:32:13 -0700

    Adds definition and assignment instructions

--- JavaScript_Course/Project1_expressions_alert/Concatenation.js
+++ JavaScript_Course/Project1_expressions_alert/Concatenation.js
@@ -1,1 +1,13 @@
+// Concatenate means to connect things together, like links in a chain. It means to take one piece of data and stick it on the end of another piece of data.
+
+// For example: concatenating the string “device” and the string “s” makes the text “devices.”
+
+// To concatenate a string in JavaScript, you use the + operator.
+
+
+
+// CONCATENATION ASSIGNMENT
+
+// Concatenate a string within the document.write() method. 
+
 document.write('"Be who you are and say what you feel,' + ' because those who midn don\'t matter and those who matter don\'t mind."' + ' - Dr. Seuss')
