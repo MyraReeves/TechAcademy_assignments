40e7450bdb030078a5bc832c1b6713a9249c687e
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-05-07 20:27:27 -0700

    Adds assignment instructions

--- JavaScript_Course/Project1_expressions_alert/Challenges/challenge1.js
+++ JavaScript_Course/Project1_expressions_alert/Challenges/challenge1.js
@@ -1,2 +1,9 @@
+// STRING ASSIGNMENT
+
+// Assign a string value to a variable.
+
+// Use the window.alert() method to display the variable.
+
+
 let ryanGeorge = 'Hi there, hello!';
 window.alert(ryanGeorge);
