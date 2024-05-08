c6e3c80f000a70180d370a4dd11b98b4d13b61fa
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-05-07 20:39:27 -0700

    Adds comment to top

--- JavaScript_Course/Project1_expressions_alert/MultipleVariables.js
+++ JavaScript_Course/Project1_expressions_alert/MultipleVariables.js
@@ -1,3 +1,5 @@
+// You can also assign multiple variables in one statement as follows:
+
 const Family = 'The Arezzinis', Dad = 'Jeremiah', Mom = 'Hermoine', Daughter = 'Penny', Son = 'Zorro';
 
 document.write(Dad);
