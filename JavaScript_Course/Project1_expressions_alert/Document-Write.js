e7853b9d36bcd49d2f0120758dfd6d3e21844ed6
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-05-07 20:10:54 -0700

    Adds comments

--- JavaScript_Course/Project1_expressions_alert/Document-Write.js
+++ JavaScript_Course/Project1_expressions_alert/Document-Write.js
@@ -1,7 +1,29 @@
+// Another way to display text using JavaScript is the document.write() method:
+
 document.write('Hello, world!');
 
+// A common action performed in coding is to assign variables.  Variables are key in every programming language.  To put the definition simply, it's a value that can be altered, depending on codnitions or data passed to the program.
+
+// In mathematics, an operator is a symbol used to carry out a computation. There are several different kinds of operators. Arithmetic operators such as +, -, /, *, and % are used to perform math functions.”
+
+// Operators such as >=, ==, and != are used to compare values. Logical operators like “and,” “or,” and “not” are used to evaluate whether an expression is true or false. In JavaScript, the assignment operator is “=”; it is used to assign a value to a variable.
+
+// For example, to say that X = 10 in JavaScript, we are assigning the variable “X” the value “10.”
+
+// To assign a variable in JavaScript we write:
+
 var X = "Hello, world!";
+
+// We assigned variable X to string value “Hello, world!”
+
+// If we then wanted to display the string “Hello, world!” using our variable we would write:
+
 document.write(X);
+
+
+
+
+// Example #2:
 
 var A = 'This is a string';
 document.write(A);
