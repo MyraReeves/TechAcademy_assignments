be936a9a5c1c3b41d2efb962cc6073d6a4a8e132
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-05-07 20:26:00 -0700

    Fixes typo

--- JavaScript_Course/Project1_expressions_alert/EscapeCharacters.js
+++ JavaScript_Course/Project1_expressions_alert/EscapeCharacters.js
@@ -3,4 +3,4 @@
 // Write a string that contains an apostrophe and/or quotation marks. Ensure to escape the applicable character(s).
 
 
-document.write('Lisa told Bart, "Knock it off, Bart! Or I\'ll tell dad!" <br> "Eat my shorts" Bard replied!');
+document.write('Lisa told Bart, "Knock it off, Bart! Or I\'ll tell dad!" <br> "Eat my shorts" Bart replied!');
