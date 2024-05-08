eb0c55e5805c409894c26d3dc5294a92f2c467e5
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-05-07 19:46:35 -0700

    Adds notes about alert boxes

---
+++ JavaScript_Course/Project1_expressions_alert/alert.js
@@ -0,0 +1,7 @@
+// Strings are one of the seven value types. In JavaScript, we can create strings with double quotes (“) or single quotes (‘), such as: “Mary had a little lamb,” or ‘Its fleece was white as snow.’  It doesn’t matter whether you use double or single quotes; they both perform the same functions.
+
+// In the following code, “Hello, World!” is a string.
+
+alert("Hello, World!");
+
+// The alert() method causes a box to display with text on it – it includes an “OK” button.
