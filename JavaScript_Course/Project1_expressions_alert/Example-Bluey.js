da4d5c5678b3a64317d95d6e1d8219fa8e4a9cc6
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-05-07 20:55:46 -0700

    Initial commit

---
+++ JavaScript_Course/Project1_expressions_alert/Example-Bluey.js
@@ -0,0 +1,3 @@
+const Characters = 'The Heeler Family', Dad = 'Bandit', Mum = 'Chilli', Eldest = 'Bluey', Youngest = 'Bingo', Uncle1 = 'Stripe', Cousin1 = 'Muffin', Cousin2 = 'Socks', Aunt1 = 'Trixie', Uncle2 = 'Radley', Aunt2 = 'Frisky';
+
+document.write(Eldest);
