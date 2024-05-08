35970c3e9bcdefbcd873c0bd26870ac0ede83507
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-05-07 20:36:49 -0700

    Increases length of the concatenated string

--- JavaScript_Course/Project1_expressions_alert/Challenges/challenge2.js
+++ JavaScript_Course/Project1_expressions_alert/Challenges/challenge2.js
@@ -1,4 +1,4 @@
 // Assign a concatenated string value to a variable and display it in the browser.
 
-let B = 'Concatenated' + 'string';
+let B = 'This ' + 'is ' + 'a' + ' concatenated' + ' string';
 document.write(B);
